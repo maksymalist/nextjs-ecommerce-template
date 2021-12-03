c6d98843aa5e28a08d49945cf0b830c9337c6a44
John Doe <79988159+John8790909@users.noreply.github.com>
2021-12-03 18:47:04 -0500

    fix 4

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -2,46 +2,55 @@ import { Button, Typography } from "@mui/material";
 import React from "react";
 import styles from "../../styles/Product.module.css";
 import AddToCart from "../../components/AddToCart";
-import axios from "axios";
+import products from "../../products.json";
 
-export default function product({
-  title,
-  price,
-  image,
-  description,
-  images,
-  id,
-}) {
+import { useRouter } from "next/router";
+
+export default function product() {
+  const router = useRouter();
+  const { id } = router.query;
   return (
     <div className={styles.main}>
       <div className={styles.wrap}>
         <div style={{ flex: "50%" }}>
           <div>
             <div className={styles.smallCard}>
-              <img className={styles.image} src={image} alt={title} />
+              <img
+                className={styles.image}
+                src={products[id] ? products[id].image : ""}
+                alt={products[id] ? products[id].title : ""}
+              />
             </div>
           </div>
           <div className={styles.wrap}>
-            {images.map((image, index) => {
-              return (
-                <div className={styles.smallCard} key={index}>
-                  <img
-                    style={{ width: "100px", height: "100px" }}
-                    className={styles.image}
-                    src={image}
-                    alt={title}
-                  />
-                </div>
-              );
-            })}
+            {products[id]
+              ? products[id].images.map((image, index) => {
+                  return (
+                    <div className={styles.smallCard} key={index}>
+                      <img
+                        style={{ width: "100px", height: "100px" }}
+                        className={styles.image}
+                        src={image}
+                        alt={products[id] ? products[id].title : ""}
+                      />
+                    </div>
+                  );
+                })
+              : null}
           </div>
         </div>
         <div style={{ flex: "50%" }} className={styles.card}>
           <div className={styles.subCard}>
             <div>
-              <Typography variant="h4">{title}</Typography>
-              <Typography variant="h5">${price}</Typography>
-              <Typography variant="h6">{description}</Typography>
+              <Typography variant="h4">
+                {products[id] ? products[id].title : ""}
+              </Typography>
+              <Typography variant="h5">
+                ${products[id] ? products[id].price : ""}
+              </Typography>
+              <Typography variant="h6">
+                {products[id] ? products[id].description : ""}
+              </Typography>
             </div>
 
             <div>
@@ -52,34 +61,4 @@ export default function product({
       </div>
     </div>
   );
-}
-
-export async function getStaticProps({ params }) {
-  const res = await axios.get(`http://localhost:3000/api/product/${params.id}`);
-  const prod = res.data;
-
-  return {
-    props: {
-      title: prod.name,
-      price: prod.price,
-      image: prod.image,
-      description: prod.description,
-      images: prod.images,
-      id: prod.id,
-    },
-  };
-}
-
-export async function getStaticPaths() {
-  const res = await axios.get("http://localhost:3000/api/products");
-  const products = res.data;
-
-  const paths = products.map((product) => ({
-    params: { id: product.id },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
 }
