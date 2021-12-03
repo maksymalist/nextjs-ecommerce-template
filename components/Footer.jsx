d733fd12c4264251c1c3d126e8ccfb3362f79794
John Doe <79988159+John8790909@users.noreply.github.com>
2021-12-03 17:45:42 -0500

    fixed <a> => <Link>

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <Link href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
         Powered by{" "}
         <span className={styles.logo}>
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
-      </a>
+      </Link>
     </footer>
   );
 }
