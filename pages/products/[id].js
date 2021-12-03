c6175d790b47801cbadc163a73b1da354591e9d9
John Doe <79988159+John8790909@users.noreply.github.com>
2021-12-03 18:54:32 -0500

    fix 5

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,14 +1,10 @@
-import { Button, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import React from "react";
 import styles from "../../styles/Product.module.css";
 import AddToCart from "../../components/AddToCart";
 import products from "../../products.json";
 
-import { useRouter } from "next/router";
-
-export default function product() {
-  const router = useRouter();
-  const { id } = router.query;
+export default function product({ id }) {
   return (
     <div className={styles.main}>
       <div className={styles.wrap}>
@@ -61,4 +57,12 @@ export default function product() {
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps({ params }) {
+  return {
+    props: {
+      id: params.id,
+    },
+  };
 }
