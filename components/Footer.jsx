6f6892a67801790873596bee3db0ce4756557ce9
John Doe <79988159+John8790909@users.noreply.github.com>
2021-12-03 17:59:03 -0500

    fix 1

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,12 +6,10 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-        Powered by{" "}
-        <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </Link>
+      Powered by{" "}
+      <span className={styles.logo}>
+        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      </span>
     </footer>
   );
 }
